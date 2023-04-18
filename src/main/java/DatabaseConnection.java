44a0e8b103b4999fc7660367a1bdc3ce0300b4b7
ShaneBaxter831 <smbaxter@cpp.edu>
2023-04-18 12:15:29 -0700

    finished all question 2 queries

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -11,7 +11,7 @@ public class DatabaseConnection {
     }
 
     public static void getTripFromDB(String start, String dest, Connection conn) throws SQLException{
-        String query = "SELECT T.startlocationname, T.destinationname, TOF.date, TOF.scheduledstarttime, TOF.scheduledarrivaltime, TOF.drivername, TOF.busid FROM TRIP AS T, TRIPOFFERING AS TOF WHERE T.tripnumber = TOF.tripnumber AND T.startlocationname = " + "'" + start +  "'" + " AND T.destinationname = " + "'" + dest + "';";
+        String query = "SELECT T.startlocationname, T.destinationname, TOF.tripdate, TOF.scheduledstarttime, TOF.scheduledarrivaltime, TOF.drivername, TOF.busid FROM TRIP AS T, TRIPOFFERING AS TOF WHERE T.tripnumber = TOF.tripnumber AND T.startlocationname = " + "'" + start +  "'" + " AND T.destinationname = " + "'" + dest + "';";
 
         try (Statement stmt = conn.createStatement()) {
             ResultSet rs = stmt.executeQuery(query);
@@ -22,13 +22,117 @@ public class DatabaseConnection {
                 System.out.print(rs.getString(4) + ", ");
                 System.out.print(rs.getString(5) + ", ");
                 System.out.print(rs.getString(6) + ", ");
-                System.out.print(rs.getString(7) + ", ");
+                System.out.print(rs.getString(7) + "\n");
             }
             rs.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
+
+    }
+
+    public static void deleteTripFromDB(String tripNum, String date, String time, Connection conn) throws SQLException {
+        String query = "DELETE FROM TRIPOFFERING " +
+                "WHERE tripnumber = " + tripNum + " AND " +
+                "tripdate = '" + date + "' AND " +
+                "ScheduledStartTime = '" + time + "'";
+
+        String afterDelete = "SELECT * FROM TRIPOFFERING";
+
+        try(Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(query);
+            ResultSet rs = stmt.executeQuery(afterDelete);
+            System.out.println("THE TABLE AFTER DELETION: ");
+            while(rs.next()){
+                System.out.print(rs.getString(1) + ", ");
+                System.out.print(rs.getString(2) + ", ");
+                System.out.print(rs.getString(3) + ", ");
+                System.out.print(rs.getString(4) + ", ");
+                System.out.print(rs.getString(5) + ", ");
+                System.out.print(rs.getString(6) + "\n");
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addTripOffering(String tripNum, String date, String startTime, String arrivTime, String driverName, String busID, Connection conn) throws SQLException {
+        String query = "INSERT INTO TRIPOFFERING VALUES ('" + tripNum + "', '" + date + "', '" + startTime + "', '" + arrivTime + "', '" + driverName + "', '" + busID + "')";
+
+        String afterInsert = "SELECT * FROM TRIPOFFERING";
+
+        try(Statement stmt = conn.createStatement()){
+            stmt.executeUpdate(query);
+            ResultSet rs = stmt.executeQuery(afterInsert);
+            System.out.println("THE TABLE AFTER INSERTION: ");
+            while(rs.next()){
+                System.out.print(rs.getString(1) + ", ");
+                System.out.print(rs.getString(2) + ", ");
+                System.out.print(rs.getString(3) + ", ");
+                System.out.print(rs.getString(4) + ", ");
+                System.out.print(rs.getString(5) + ", ");
+                System.out.print(rs.getString(6) + "\n");
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void changeTripDriver(String tripNum, String date, String startTime, String driverName, Connection conn) throws SQLException {
+        String query = "UPDATE TRIPOFFERING " +
+                "SET drivername = '" + driverName + "' " +
+                "WHERE tripnumber = '" + tripNum + "' AND " +
+                "tripdate = '" + date + "' AND " +
+                "scheduledstarttime = '" + startTime + "'";
+
+        String afterUpdate = "SELECT * FROM TRIPOFFERING";
+
+        try(Statement stmt = conn.createStatement()){
+            stmt.executeUpdate(query);
+            ResultSet rs = stmt.executeQuery(afterUpdate);
+            System.out.println("THE TABLE AFTER UPDATE: ");
+            while(rs.next()){
+                System.out.print(rs.getString(1) + ", ");
+                System.out.print(rs.getString(2) + ", ");
+                System.out.print(rs.getString(3) + ", ");
+                System.out.print(rs.getString(4) + ", ");
+                System.out.print(rs.getString(5) + ", ");
+                System.out.print(rs.getString(6) + "\n");
+            }
+            rs.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void changeTripBusID(String tripNum, String date, String startTime, String busID, Connection conn) throws SQLException {
+        String query = "UPDATE TRIPOFFERING " +
+                "SET busID = '" + busID + "' " +
+                "WHERE tripnumber = '" + tripNum + "' AND " +
+                "tripdate = '" + date + "' AND " +
+                "scheduledstarttime = '" + startTime + "'";
+
+        String afterUpdate = "SELECT * FROM TRIPOFFERING";
+
+        try(Statement stmt = conn.createStatement()){
+            stmt.executeUpdate(query);
+            ResultSet rs = stmt.executeQuery(afterUpdate);
+            System.out.println("THE TABLE AFTER UPDATE: ");
+            while(rs.next()){
+                System.out.print(rs.getString(1) + ", ");
+                System.out.print(rs.getString(2) + ", ");
+                System.out.print(rs.getString(3) + ", ");
+                System.out.print(rs.getString(4) + ", ");
+                System.out.print(rs.getString(5) + ", ");
+                System.out.print(rs.getString(6) + "\n");
+            }
+            rs.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
 
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,9 +16,13 @@ public class Main {
         }
 
         System.out.println("\nWELCOME TO THE POMONA TRANSIT SYSTEM. WHAT WOULD YOU LIKE TO DO?\n" +
-                            "==================================================================\n" +
-                            "1. DISPLAY ALL TRIPS FOR A GIVEN START LOCATION AND DESTINATION .\n" +
-                            "2. \n");
+                            "=======================================================================================\n" +
+                            "1. DISPLAY ALL TRIPS FOR A GIVEN START LOCATION AND DESTINATION.\n" +
+                            "2. DELETE A TRIP OFFERING BASED ON TRIP NUMBER, DATE, AND SCHEDULED START TIME.\n" +
+                            "3. ENTER A NEW TRIP OFFERING INTO THE SYSTEM.\n" +
+                            "4. CHANGE THE DRIVER OF A CURRENT TRIP OFFERING.\n" +
+                            "5. CHANGE THE BUS ID OF A CURRENT TRIP OFFERING \n" +
+                            "6. ");
 
         int option = myScanner.nextInt();
         if(option == 1){
@@ -35,7 +39,88 @@ public class Main {
 
         }
         if(option == 2){
-            //option 2 code
+            System.out.println("PLEASE ENTER TRIP #: ");
+            String tripNum = myScanner.next();
+            System.out.println("PLEASE ENTER DATE: ");
+            String date = myScanner.next();
+            System.out.println("PLEASE ENTER SCHEDULED START TIME: ");
+            String time = myScanner.next();
+
+            try{
+                DatabaseConnection.deleteTripFromDB(tripNum, date, time, transitConnection);
+            } catch(SQLException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        if(option == 3){
+            boolean moreInputs = true;
+            while(moreInputs) {
+                System.out.println("PLEASE ENTER TRIP #: ");
+                String tripNum = myScanner.next();
+                System.out.println("PLEASE ENTER DATE: ");
+                String date = myScanner.next();
+                System.out.println("PLEASE ENTER SCHEDULED START TIME: ");
+                String time = myScanner.next();
+                System.out.println("PLEASE ENTER SCHEDULED ARRIVAL TIME: ");
+                String arrivTime = myScanner.next();
+                System.out.println("PLEASE ENTER DRIVER NAME: ");
+                String name = myScanner.next() + " " + myScanner.next();
+                System.out.println("PLEASE ENTER BUS ID: ");
+                String busID = myScanner.next();
+
+                try {
+                    DatabaseConnection.addTripOffering(tripNum, date, time, arrivTime, name, busID, transitConnection);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("WOULD YOU LIKE TO INPUT MORE TRIPS? Y/N");
+                String response = myScanner.next();
+                if(response.equals("Y") || response.equals("y"))
+                    moreInputs = true;
+                else
+                    moreInputs = false;
+            }
+        }
+        if(option == 4){
+            System.out.println("PLEASE ENTER TRIP #: ");
+            String tripNum = myScanner.next();
+            System.out.println("PLEASE ENTER DATE: ");
+            String date = myScanner.next();
+            System.out.println("PLEASE ENTER SCHEDULED START TIME: ");
+            String time = myScanner.next();
+            System.out.println("PLEASE ENTER NEW DRIVER NAME: ");
+            String name = myScanner.next() + " " + myScanner.next();
+
+            try{
+                DatabaseConnection.changeTripDriver(tripNum, date, time, name, transitConnection);
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+
+        if(option == 5){
+            System.out.println("PLEASE ENTER TRIP #: ");
+            String tripNum = myScanner.next();
+            System.out.println("PLEASE ENTER DATE: ");
+            String date = myScanner.next();
+            System.out.println("PLEASE ENTER SCHEDULED START TIME: ");
+            String time = myScanner.next();
+            System.out.println("PLEASE ENTER NEW BUSID: ");
+            String busID = myScanner.next();
+
+            try{
+                DatabaseConnection.changeTripBusID(tripNum, date, time, busID, transitConnection);
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+
+        }
+
+        if(option == 6){
+
         }
     }
 }
